f7d05f7222dba0a332435102fc5f50c0a1a0578a
LauraAguirre <laurafelicitasar@gmail.com>
2024-05-21 15:54:49 -0400

    mi primer commit

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem(){
+    return(
+      <li>
+        <span>V</span>
+        <p>dar de comer a perrito</p>
+        <span>X</span>
+      </li>
+    );
+  }
+
+export {TodoItem};
---
+++ src/TodoSearch.js
@@ -0,0 +1,7 @@
+function TodoSearch(){
+    return(
+      <input placeholder="dar de comer a Barry"/>
+    );
+  }
+
+  export {TodoSearch};
